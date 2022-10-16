db2da4f5f0ed6c6646c892d87a4bd039b0e2b75b
Mohamed Roshdy <62522577+PASHAMESER@users.noreply.github.com>
2022-10-16 16:10:43 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React from "./css/App.css";
+// import HomePageFaceBook from './html/homePageFaceBook.html';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          
+      </header>
+      <div className="dev" >Hello React Js</div>
+    </div>
+  );
+}
+
+export default App ;
+// export default HomePageFaceBook ;
