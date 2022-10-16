8a2d15aa672b926f81b4eb38310988eaf3212033
Mohamed Roshdy <62522577+PASHAMESER@users.noreply.github.com>
2022-10-16 17:11:49 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,9 @@ import React from "./css/App.css";
 // import HomePageFaceBook from './html/homePageFaceBook.html';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          
-      </header>
-      <div className="dev" >Hello React Js</div>
-    </div>
+    <>
+    <div className="dev" >Hello React Js</div>
+    </>
   );
 }
 
